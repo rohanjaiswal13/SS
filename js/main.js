69a7a5b1ebe65ed30686a795f75743e651b915a8
Rohan Jaiswal <rj957977@gmail.com>
2024-06-25 21:59:42 +0530

    autoplay

--- js/main.js
+++ js/main.js
@@ -41,26 +41,6 @@
    $.Scrollax();
 
 	var carousel = function() {
-		$('.home-slider').owlCarousel({
-	    margin:0,
-	    animateOut: 'fadeOut',
-	    animateIn: 'fadeIn',
-	    nav:false,
-	    autoplayHoverPause: false,
-	    items: 1,
-	    navText : ["<span class='ion-md-arrow-back'></span>","<span class='ion-chevron-right'></span>"],
-	    responsive:{
-	      0:{
-	        items:1
-	      },
-	      600:{
-	        items:1
-	      },
-	      1000:{
-	        items:1
-	      }
-	    }
-		});
 		$('.carousel-testimony').owlCarousel({
 			center: true,
 			loop: true,
@@ -84,6 +64,7 @@
 		$('.carousel-client').owlCarousel({
 			center: true,
 			loop: true,
+			autoplay: true,
 			items:1,
 			margin: 30,
 			stagePadding: 0,
@@ -324,3 +305,19 @@
 
 })(jQuery);
 
+$(document).ready(function () {
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 20) {
+			$('#toTopBtn').fadeIn();
+		} else {
+			$('#toTopBtn').fadeOut();
+		}
+	});
+
+	$('#toTopBtn').click(function () {
+		$("html, body").animate({
+			scrollTop: 0
+		}, 1000);
+		return false;
+	});
+});
