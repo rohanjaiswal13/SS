d2a40f79fa051c17843e741116d0edeaafdfaf7c
Rohan Jaiswal <rj957977@gmail.com>
2024-06-26 15:34:09 +0530

    fix error

--- js/main.js
+++ js/main.js
@@ -41,6 +41,26 @@
    $.Scrollax();
 
 	var carousel = function() {
+		$('.home-slider').owlCarousel({
+			margin:0,
+			animateOut: 'fadeOut',
+			animateIn: 'fadeIn',
+			nav:false,
+			autoplayHoverPause: false,
+			items: 1,
+			navText : ["<span class='ion-md-arrow-back'></span>","<span class='ion-chevron-right'></span>"],
+			responsive:{
+			  0:{
+				items:1
+			  },
+			  600:{
+				items:1
+			  },
+			  1000:{
+				items:1
+			  }
+			}
+			});
 		$('.carousel-testimony').owlCarousel({
 			center: true,
 			loop: true,
